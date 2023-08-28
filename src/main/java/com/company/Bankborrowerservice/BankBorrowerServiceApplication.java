0a68129c8fdfe42647bd075ca9665ca34a80e746
AkhmatovSardor <sardor.akhmatov0969@gmail.com>
2023-08-28 14:43:30 +0500

    first commit

---
+++ src/main/java/com/company/Bankborrowerservice/BankBorrowerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.company.Bankborrowerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankBorrowerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankBorrowerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/Bankborrowerservice/BankBorrowerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.Bankborrowerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankBorrowerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
